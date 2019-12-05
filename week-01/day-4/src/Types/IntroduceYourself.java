9bcc94c7017b144001c28de0b159ce408947080c
Barnabas Szabo <barna.efs@gmail.com>
2019-12-05 13:08:22 +0100

    Introduce yourself

---
+++ week-01/day-4/src/Types/IntroduceYourself.java
@@ -0,0 +1,10 @@
+package Types;
+
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("My name is: " + "Barna");
+        System.out.println("My age is: " + "26");
+        System.out.println("My height is: " +1.7+ "m");
+
+    }
+}
