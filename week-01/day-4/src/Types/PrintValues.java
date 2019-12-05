a77f44ec0c9d9fa91c857497e1441cb59c333fca
Barnabas Szabo <barna.efs@gmail.com>
2019-12-05 12:34:01 +0100

    Print values with different types

---
+++ week-01/day-4/src/Types/PrintValues.java
@@ -0,0 +1,9 @@
+public class PrintValues {
+    public static void main(String[]args) {
+        System.out.println("Halihó!");
+
+        System.out.println(42);
+
+        System.out.println(3.1411592);
+    }
+}
