a251f186ca0a868e2a9c09567bd6e83e9f7df8a4
Barnabas Szabo <barna.efs@gmail.com>
2019-12-05 15:06:32 +0100

    Basic Info

---
+++ week-01/day-4/src/Variables/DefineBasicInfo.java
@@ -0,0 +1,17 @@
+package Variables;
+
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String myName= "Barnabas Szabo";
+        System.out.println(myName);
+
+        int myAge=26;
+        System.out.println(myAge);
+
+        double myHeight=1.70;
+        System.out.println(myHeight);
+
+        boolean marriedOrNot=false;
+        System.out.println(marriedOrNot);
+    }
+}
