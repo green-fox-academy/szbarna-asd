f90261ccd2b14cf976037925681a636d1d9f3d19
Barnabas Szabo <barna.efs@gmail.com>
2019-12-05 13:34:40 +0100

    Coding Hours part1

---
+++ week-01/day-4/src/CoodingHours.java
@@ -0,0 +1,6 @@
+public class CoodingHours {
+    public static void main(String[] args) {
+        System.out.println("Coded Hours/" + "Attendee/" + "Semester= " +(5*15*6) + "Hours");
+        
+    }
+}
