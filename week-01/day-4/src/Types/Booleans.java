6ca72edae6bcb68cb0c2753c5c9f5ecbfc134ce5
Barnabas Szabo <barna.efs@gmail.com>
2019-12-05 12:57:30 +0100

    Booleiens

---
+++ week-01/day-4/src/Types/Booleans.java
@@ -0,0 +1,21 @@
+package Types;
+
+public class Booleans {
+    public static void main(String[] args) {
+        System.out.println(true);
+        System.out.println(false);
+
+        System.out.println(!true);
+        System.out.println(!false);
+
+        System.out.println(true && true);
+        System.out.println(true && false);
+        System.out.println(false && true);
+        System.out.println(false && false);
+
+        System.out.println(true || true);
+        System.out.println(true || false);
+        System.out.println(false || true);
+        System.out.println(false || false);
+    }
+}
