6dfa8f8d422fd45fc5e6fb42674b04ca8362b672
Barnabas Szabo <barna.efs@gmail.com>
2019-12-05 12:52:07 +0100

    Nember Operations

---
+++ week-01/day-4/src/Types/Floats.java
@@ -0,0 +1,16 @@
+package Types;
+
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+public class Floats {
+    public static void main(String[] args) {
+        System.out.println(3.145);
+        System.out.println(-1.5);
+        System.out.println(0.0);
+
+
+        System.out.println(.5);
+        System.out.println(5.);
+
+    }
+}
---
+++ week-01/day-4/src/Types/Integers.java
@@ -0,0 +1,11 @@
+package Types;
+
+public class Integers {
+    public static void main(String[] args) {
+        System.out.println(42);
+        System.out.println(-1);
+        System.out.println(0);
+
+        System.out.println(2123425423);
+    }
+}
