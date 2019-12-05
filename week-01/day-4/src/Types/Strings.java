69d458787a0194f052b767ba146587bde28c8ca8
Barnabas Szabo <barna.efs@gmail.com>
2019-12-05 13:03:34 +0100

    strings

---
+++ week-01/day-4/src/Types/Strings.java
@@ -0,0 +1,14 @@
+package Types;
+
+public class Strings {
+    public static void main(String[] args) {
+        System.out.println("apple");
+        System.out.println("don't");
+        System.out.println("\"Everything you can imagine is real.\" -Picasso");
+
+        System.out.println("tooth" + "brush");
+
+        System.out.println("My favourite number is: " +8);
+
+    }
+}
