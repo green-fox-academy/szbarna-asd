0c5135db6360d41e49c82f8e6bb5f2f997052175
Barnabas Szabo <barna.efs@gmail.com>
2019-12-05 14:06:35 +0100

    Favourite Number

---
+++ week-01/day-4/src/Variables/CreatingDifferentVariablesEachType.java
@@ -0,0 +1,30 @@
+package Variables;
+
+public class CreatingDifferentVariablesEachType {
+    public static void main(String[] args) {
+        String welcome = "Hello, World";
+        System.out.println(welcome);
+
+        boolean isAwesome = true;
+        System.out.println(isAwesome);
+
+        int theMEaningOfLifeAndTheUniverseAndEverything =42;
+        System.out.println(theMEaningOfLifeAndTheUniverseAndEverything);
+
+        double pi =3.141592;
+        System.out.println(pi);
+
+        byte b=2;
+        short s=567;
+        long l=300000000000000L;
+        float f=1.3f;
+        float f2=45f;
+
+        int number;
+
+        number=12;
+
+        number=23;
+
+    }
+}
---
+++ week-01/day-4/src/Variables/FavouriteNumber.java
@@ -0,0 +1,8 @@
+package Variables;
+
+public class FavouriteNumber {
+    public static void main(String[] args) {
+        int favouriteNumber=7;
+        System.out.println("My favourite number is: " + favouriteNumber);
+    }
+}
