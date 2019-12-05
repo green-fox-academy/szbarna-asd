1cda8eb80224ec891130a7a4d877d364091f1987
Barnabas Szabo <barna.efs@gmail.com>
2019-12-05 11:56:11 +0100

    HelloWorld

--- week-01/day-4/src/HelloWorld/HelloMe.java
+++ week-01/day-4/src/HelloWorld/HelloMe.java
@@ -1,3 +1,5 @@
+package HelloWorld;
+
 public class HelloMe {
     public static void main(String[] args) {
         System.out.println("hello, me!");
--- week-01/day-4/src/HelloWorld/HelloOthers.java
+++ week-01/day-4/src/HelloWorld/HelloOthers.java
@@ -1,3 +1,5 @@
+package HelloWorld;
+
 public class HelloOthers {
     public static void main(String[] args) {
         System.out.println("Hello, Others!");
