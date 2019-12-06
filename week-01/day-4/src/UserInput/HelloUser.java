20b43440dbc6e7471a70b13a1bd50019f9474762
Barnabas Szabo <barna.efs@gmail.com>
2019-12-06 09:12:30 +0100

    Hello User

--- week-01/day-4/src/UserInput/HelloUser.java
+++ week-01/day-4/src/UserInput/HelloUser.java
@@ -1,6 +1,17 @@
 package UserInput;
 
-public class HelloUser { 
+import java.util.Scanner;
 
+public class HelloUser {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        String userName;
 
+        System.out.println("Hello, User");
+        System.out.println("What's your name?");
+
+        userName = sc.nextLine();
+
+        System.out.println("Your name is: " + userName);
+    }
 }
