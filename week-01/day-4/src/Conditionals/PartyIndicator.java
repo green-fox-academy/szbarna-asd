a87e6b10b979dacf6c1f7ee1803854518419a9c5
Barnabas Szabo <barna.efs@gmail.com>
2019-12-06 15:11:35 +0100

    Party Indicator

--- week-01/day-4/src/Conditionals/PartyIndicator.java
+++ week-01/day-4/src/Conditionals/PartyIndicator.java
@@ -4,24 +4,27 @@ import java.util.Scanner;
 
 public class PartyIndicator {
     public static void main(String[] args) {
-        System.out.println("I heard you will have a party. :) How many girls will be present?");
-        Scanner girls = new Scanner(System.in);
+        for (int i = 0; i <4 ; i++) { //A program ellenőrzéséhez, hogy ne kelljen minden eyes új értéknél újraindítani a programot
+            System.out.println("I heard you will have a party. :) How many girls will be present?");
+            Scanner girls = new Scanner(System.in);
 
-        int numberOfGirls = girls.nextInt();
+            int numberOfGirls = girls.nextInt();
 
-        System.out.println("How many boys will be present?");
-        Scanner boys = new Scanner(System.in);
+            System.out.println("How many boys will be present?");
+            Scanner boys = new Scanner(System.in);
 
-        int numberOfBoys = boys.nextInt();
+            int numberOfBoys = boys.nextInt();
 
-        if (numberOfBoys == numberOfGirls & (numberOfBoys + numberOfGirls) >= 20) {
-            System.out.println("The party is gonna be excellent! ;)");
-        } else if ((numberOfBoys + numberOfGirls) >=20 && (numberOfBoys != numberOfGirls)) {
-            System.out.println("The party is gonna be quite cool! :)");
-        } else if ((numberOfBoys + numberOfGirls) < 20) {
-            System.out.println("That's gonna be an average party... :|");
-        } else if (numberOfGirls <= 0) {
-            System.out.println("Oh god! It's gonna be a sausage party... :(");
+            if (numberOfBoys == numberOfGirls & (numberOfBoys + numberOfGirls) >= 20) {
+                System.out.println("The party is gonna be excellent! ;)");
+            } else if ((numberOfBoys + numberOfGirls) >=20 && (numberOfBoys != numberOfGirls)) {
+                System.out.println("The party is gonna be quite cool! :)");
+            } else if ((numberOfBoys + numberOfGirls) < 20) {
+                System.out.println("That's gonna be an average party... :|");
+            } else if (numberOfGirls <= 0) {
+                System.out.println("Oh god! It's gonna be a sausage party... :(");
+            }
         }
+
     }
 }
