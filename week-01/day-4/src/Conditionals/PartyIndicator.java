ce9f24368f13a40a676e1079c70763905edfcbe4
Barnabas Szabo <barna.efs@gmail.com>
2019-12-06 11:15:01 +0100

    Party Indicator/1

---
+++ week-01/day-4/src/Conditionals/PartyIndicator.java
@@ -0,0 +1,27 @@
+package Conditionals;
+
+import java.util.Scanner;
+
+public class PartyIndicator {
+    public static void main(String[] args) {
+        System.out.println("I heard you will have a party. :) How many girls will be present?");
+        Scanner girls = new Scanner(System.in);
+
+        int numberOfGirls = girls.nextInt();
+
+        System.out.println("How many boys will be present?");
+        Scanner boys = new Scanner(System.in);
+
+        int numberOfBoys = boys.nextInt();
+
+        if (numberOfBoys == numberOfGirls && (numberOfBoys + numberOfGirls) >20) {
+            System.out.println("The party is gonna be excellent! ;)");
+        //} else if ((numberOfBoys + numberOfGirls) > 20 && (numberOfBoys != numberOfGirls)) {
+            System.out.println("The party is gonna be quite cool! :)");
+        //} else if ((numberOfBoys + numberOfGirls) < 20) {
+            System.out.println("That's gonna be an avarage party... :|");
+        //} else if (numberOfGirls <= 0) {
+            System.out.println("Oh god! It's gonna be a sausage party... :(");
+        }
+    }
+}
