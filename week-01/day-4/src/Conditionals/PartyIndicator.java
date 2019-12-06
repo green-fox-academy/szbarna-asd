27f06d709af17ca67b5d94160acec13156700351
Barnabas Szabo <barna.efs@gmail.com>
2019-12-06 15:57:54 +0100

    Triangle

--- week-01/day-4/src/Conditionals/PartyIndicator.java
+++ week-01/day-4/src/Conditionals/PartyIndicator.java
@@ -7,20 +7,20 @@ public class PartyIndicator {
         for (int i = 0; i <4 ; i++) { //A program ellenőrzéséhez, hogy ne kelljen minden eyes új értéknél újraindítani a programot
 
             System.out.println("I heard you will have a party. :) How many girls will be present?");
-            Scanner girls = new Scanner(System.in);
+            Scanner girls = new Scanner(System.in); //egy programban elég egy scanner --> így Scanner scanner = new
             int numberOfGirls = girls.nextInt();
 
             System.out.println("How many boys will be present?");
             Scanner boys = new Scanner(System.in);
             int numberOfBoys = boys.nextInt();
 
-            if (numberOfBoys == numberOfGirls & (numberOfBoys + numberOfGirls) >= 20) {
+            if (numberOfBoys == numberOfGirls & (numberOfBoys + numberOfGirls) >= 20) { //kondíciók esetén figyelni a logikai sorrender!!
                 System.out.println("The party is gonna be excellent! ;)");
             } else if ((numberOfBoys + numberOfGirls) >=20 && (numberOfBoys != numberOfGirls)) {
                 System.out.println("The party is gonna be quite cool! :)");
             } else if ((numberOfBoys + numberOfGirls) < 20) {
                 System.out.println("That's gonna be an average party... :|");
-            } else if (numberOfGirls <= 0) {
+            } else if (numberOfGirls == 0) {
                 System.out.println("Oh god! It's gonna be a sausage party... :(");
             }
         }
--- week-01/day-4/src/Loops/DrawTriangle.java
+++ week-01/day-4/src/Loops/DrawTriangle.java
@@ -1,7 +1,18 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class DrawTriangle {
     public static void main(String[] args) {
+        System.out.println("Please type an integer number. Which number, it's up to you. :)");
+        Scanner sc = new Scanner(System.in);
+        int yourNumber = sc.nextInt();
 
+        for (int i = 0; i < yourNumber; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
