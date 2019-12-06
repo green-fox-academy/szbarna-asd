63dddeee85183d239ad64321ef4c2aed7371ec7d
Barnabas Szabo <barna.efs@gmail.com>
2019-12-06 15:15:32 +0100

    Party Indicator

--- week-01/day-4/src/Conditionals/PartyIndicator.java
+++ week-01/day-4/src/Conditionals/PartyIndicator.java
@@ -5,14 +5,13 @@ import java.util.Scanner;
 public class PartyIndicator {
     public static void main(String[] args) {
         for (int i = 0; i <4 ; i++) { //A program ellenőrzéséhez, hogy ne kelljen minden eyes új értéknél újraindítani a programot
+
             System.out.println("I heard you will have a party. :) How many girls will be present?");
             Scanner girls = new Scanner(System.in);
-
             int numberOfGirls = girls.nextInt();
 
             System.out.println("How many boys will be present?");
             Scanner boys = new Scanner(System.in);
-
             int numberOfBoys = boys.nextInt();
 
             if (numberOfBoys == numberOfGirls & (numberOfBoys + numberOfGirls) >= 20) {
---
+++ week-01/day-4/src/Loops/DrawTriangle.java
@@ -0,0 +1,7 @@
+package Loops;
+
+public class DrawTriangle {
+    public static void main(String[] args) {
+
+    }
+}
