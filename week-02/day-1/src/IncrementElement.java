87b2c717b4989dc928acbb2c42a0bf01336de08f
Barnabas Szabo <barna.efs@gmail.com>
2019-12-09 14:34:17 +0100

    Print All

---
+++ week-02/day-1/src/IncrementElement.java
@@ -0,0 +1,11 @@
+public class IncrementElement {
+    public static void main(String[] args) {
+        /* - Create an array variable named `t`
+        with the following content: `[1, 2, 3, 4, 5]`
+        - Increment the third element
+                - Print the third element*/
+       int[] t = {1, 2, 3, 4, 5};
+       t[4] += 1; // += és a növelés értéke
+       System.out.println(t[4]);
+    }
+}
---
+++ week-02/day-1/src/PrintAll.java
@@ -0,0 +1,12 @@
+public class PrintAll {
+    public static void main(String[] args) {
+        // - Create an array variable named `numbers`
+        //   with the following content: `[4, 5, 6, 7]`
+        // - Print all the elements of `numbers`
+        int[] numbers = {4, 5, 6, 7};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " "); //print -sorban írja ki; println -vonalban írja ki
+        }
+    }
+}
