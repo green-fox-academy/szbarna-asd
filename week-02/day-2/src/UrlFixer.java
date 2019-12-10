183a402c4c8c6bd82c40614cf336501ab9f70f31
Barnabas Szabo <barna.efs@gmail.com>
2019-12-10 13:02:05 +0100

    Url Fixer

---
+++ week-02/day-2/src/UrlFixer.java
@@ -0,0 +1,15 @@
+public class UrlFixer {
+    public static void main(String... args){
+        String url = "https//www.reddit.com/r/nevertellmethebots";
+
+        // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+        // Also, the URL is missing a crutial component, find out what it is and insert it too!
+        // Try to solve it more than once using different String functions!
+        String fixedUrl = url.replace("bots" , "odds");
+        fixedUrl = fixedUrl.replace("s/" , "s:/"); //replacement
+        System.out.println(fixedUrl);
+
+        String fixedUrl2 = "https:" + url.substring(5, url.length() - 4) + "odds"; // substring az adott
+        System.out.println(fixedUrl2);                                              //stringben az 5. indextől a sor végétől
+    }                                                                               // számított 4. indexig printel
+}
