3821a5e0634ffeac9f6528fd447069f3d77b1ba7
Barnabas Szabo <barna.efs@gmail.com>
2019-12-17 13:06:20 +0100

    Folyosó

---
+++ week-02/day-4/src/Trees.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Trees {
+
+  public static void main(String[] args) {
+    ArrayList<String> fenyo = new ArrayList<String>();//Vagy az összes paraméterre létrehozok egy listet
+    fenyo.add("fenyő fajta");
+    fenyo.add("green");
+    fenyo.add("1");
+    fenyo.add("male");
+    ArrayList<String> tolgy = new ArrayList<String>();
+    tolgy.add("tolgy fajta");
+    tolgy.add("brown");
+    tolgy.add("13");
+    tolgy.add("female");
+    ArrayList<String> szilva = new ArrayList<String>();
+    szilva.add("szilva fajta");
+    szilva.add("dark green");
+    szilva.add("20");
+    szilva.add("femal");
+    ArrayList<String> barack = new ArrayList<String>();
+    barack.add("barack fajta");
+    barack.add("yellow");
+    barack.add("5");
+    barack.add("male");
+    ArrayList<String> fuz = new ArrayList<String>();
+    fuz.add("fuz fajta");
+    fuz.add("light green");
+    fuz.add("55");
+    fuz.add("female");
+  }
+}
