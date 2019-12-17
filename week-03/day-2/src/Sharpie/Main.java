9df335ebc79fceff912ace87313f4ee487c79e55
Barnabas Szabo <barna.efs@gmail.com>
2019-12-17 17:13:11 +0100

    Sharpie

---
+++ week-03/day-2/src/Sharpie/Main.java
@@ -0,0 +1,19 @@
+package Sharpie;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Sharpie sharpie1 = new Sharpie("Black", 10f);
+    Sharpie sharpie2 = new Sharpie("Red", 20f);
+
+    System.out.println("Details of Sharpies before using them:");
+    sharpie1.print();
+    sharpie2.print();
+
+    sharpie1.sharpie();
+    sharpie2.sharpie();
+    System.out.println("Details after using them:");
+    sharpie1.print();
+    sharpie2.print();
+  }
+}
---
+++ week-03/day-2/src/Sharpie/Sharpies.java
@@ -0,0 +1,25 @@
+package Sharpie;
+
+class Sharpie {
+  /*Create Sharpie class
+  We should know about each sharpie their color (which should be a string), width (which will be a floating point number), inkAmount (another floating point number)
+  When creating one, we need to specify the color and the width
+  Every sharpie is created with a default 100 as inkAmount
+  We can use() the sharpie objects
+  which decreases inkAmount*/
+  private String colorOfSharpie;
+  private Float widthOfSharpie;
+  private Float inkAmountOfSharpie;
+
+  public Sharpie(String colorOfSharpie, Float widthOfSharpie) {
+    this.colorOfSharpie = colorOfSharpie;
+    this.widthOfSharpie = widthOfSharpie;
+    this.inkAmountOfSharpie = 100f;
+  }
+  public void sharpie() {
+    --inkAmountOfSharpie;
+  }
+  public void print() {
+    System.out.println("Color of Sharpie:" + colorOfSharpie + ". Width of Sharpie: " + widthOfSharpie + ". Remaining amount of Sharpie's ink is:" + inkAmountOfSharpie);
+  }
+}
