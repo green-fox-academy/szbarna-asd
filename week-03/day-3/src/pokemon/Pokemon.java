e1ddf74ea3e47f6ff9460425ea756ba390a31053
Barnabas Szabo <barna.efs@gmail.com>
2019-12-18 13:46:19 +0100

    I Choose You Fzcker

---
+++ week-03/day-3/src/pokemon/Pokemon.java
@@ -0,0 +1,31 @@
+package pokemon;
+
+public class Pokemon {
+  String name;
+  String type;
+  String effectiveAgainst;
+
+  public String getName() {
+    return name;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public String getEffectiveAgainst() {
+    return effectiveAgainst;
+  }
+
+  public Pokemon(String name, String type, String effectiveAgainst) {
+    this.name = name;
+    this.type = type;
+    this.effectiveAgainst = effectiveAgainst;
+
+
+  }
+
+  boolean isEffectiveAgainst(Pokemon anotherPokemon) {
+    return this.effectiveAgainst.equals(anotherPokemon.type);
+  }
+}
