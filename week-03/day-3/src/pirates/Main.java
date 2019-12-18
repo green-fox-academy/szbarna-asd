cc4e2e2b47cee14c4bb0eb4eb38c2c6c6601e870
Barnabas Szabo <barna.efs@gmail.com>
2019-12-18 17:06:30 +0100

    Pirates With Rum Counter

---
+++ week-03/day-3/src/pirates/Main.java
@@ -0,0 +1,13 @@
+package pirates;
+
+public class Main {
+  public static void main(String[] args) {
+    Pirate Johny = new Pirate(0);
+    Johny.howItGoingMate();
+    Johny.drinkSomeRum();
+    Johny.drinkSomeRum();
+    Johny.drinkSomeRum();
+    Johny.drinkSomeRum();
+    Johny.howItGoingMate();
+  }
+}
---
+++ week-03/day-3/src/pirates/Pirate.java
@@ -0,0 +1,24 @@
+package pirates;
+
+public class Pirate {
+  private String pirate;
+  private Integer rumCounter;
+
+  public Pirate(String pirate, Integer rumCounter) {
+    this.pirate = pirate;
+    this.rumCounter = rumCounter;
+  }
+  public Pirate(Integer rumCounter) {
+    this.rumCounter = 0;
+  }
+  public void drinkSomeRum() {
+  ++rumCounter;
+  }
+  public void howItGoingMate() {
+    if(rumCounter < 4) {
+      System.out.println("Pour me anudder!");
+    } else {
+      System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+    }
+  }
+}
