475e1c501dfc8cfafa08374c8e43d566e4951f9f
Barnabas Szabo <barna.efs@gmail.com>
2020-01-07 17:29:01 +0100

    Still in The Garden

--- week-04/day-2.1/src/gardenApplication/Main.java
+++ week-04/day-2.1/src/gardenApplication/Main.java
@@ -5,16 +5,16 @@ import java.util.ArrayList;
 public class Main {
 
   public static void main(String[] args) {
-    ArrayList<Plant> plants = new ArrayList<>();
+    Garden garden = new Garden();
 
     Tree tree1 = new Tree("Blue");
-    plants.add(tree1);
     Flower flower1 = new Flower("yellow");
-    plants.add(flower1);
 
-    for (Plant plant : plants) {
+    garden.addPlants(flower1);
+    garden.addPlants(tree1);
+
+    for (Garden  :) {
       plant.getInfo();
     }
-
   }
 }
