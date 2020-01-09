e8faf7f934d71cff41bf1bd995df5478a038ab03
Barnabas Szabo <barna.efs@gmail.com>
2020-01-09 11:15:53 +0100

    Apple

---
+++ week-04/Testing/Test/AppleTest.java
@@ -0,0 +1,21 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class AppleTest {
+
+  private AppleMain main;
+
+  @Before
+  public void initialize(){
+    main = new AppleMain();
+  }
+
+  @Test
+  public void addTest() {
+    assertEquals("apple", main.getApple());
+  }
+
+}
---
+++ week-04/Testing/src/AppleMain.java
@@ -0,0 +1,12 @@
+public class AppleMain {
+
+  public static void main(String[] args) {
+
+
+  }
+
+  public String getApple() {
+
+    return "apple";
+  }
+}
