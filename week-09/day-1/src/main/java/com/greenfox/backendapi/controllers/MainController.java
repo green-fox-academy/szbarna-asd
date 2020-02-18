2f07fadc7e82321d60dd4ca6f2217a0b4c3e3130
Barnabas Szabo <barna.efs@gmail.com>
2020-02-18 09:53:29 +0100

    Greeting works

--- week-09/day-1/src/main/java/com/greenfox/backendapi/controllers/MainController.java
+++ week-09/day-1/src/main/java/com/greenfox/backendapi/controllers/MainController.java
@@ -1,6 +1,8 @@
 package com.greenfox.backendapi.controllers;
 
 import com.greenfox.backendapi.domains.Doubling;
+import com.greenfox.backendapi.domains.Greet;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+
+    Greet greet;
 
     @GetMapping("/")
     public String index() {
@@ -23,5 +27,21 @@ public class MainController {
         } else {
             return new Doubling();
         }
+    }
+
+    @GetMapping("/greeter")
+    @ResponseBody
+    public ResponseEntity<Greet> greeting(@RequestParam(required = false) String name,
+                                          @RequestParam(required = false) String title) {
+        if (name == null && title == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    greet = new Greet()
+            );
+        }else if (title == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    greet = new Greet("Please provide a title!")
+            );
+        }
+        return ResponseEntity.ok(greet = new Greet(name, title));
     }
 }
---
+++ week-09/day-1/src/main/java/com/greenfox/backendapi/domains/Greet.java
@@ -0,0 +1,38 @@
+package com.greenfox.backendapi.domains;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+public class Greet {
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String error;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String welcome_message;
+
+    public Greet(String name, String title) {
+        this.welcome_message = "Oh, hi there " + name + ", my dear " + title + "!";
+    }
+
+    public Greet() {
+        this.error = "Please provide a name and a title!";
+    }
+
+    public Greet(String error) {
+        this.error = error;
+    }
+
+    public String getWelcome_message() {
+        return welcome_message;
+    }
+
+    public void setWelcome_message(String welcome_message) {
+        this.welcome_message = welcome_message;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
